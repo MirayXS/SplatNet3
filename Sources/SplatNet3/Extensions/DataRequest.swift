e9f25a8d231aaee9d1547329c40766e4373703ec
tkgstrator <nasawake.am@gmail.com>
2023-04-01 20:19:12 +0900

    update: output request session log

--- Sources/SplatNet3/Extensions/DataRequest.swift
+++ Sources/SplatNet3/Extensions/DataRequest.swift
@@ -17,11 +17,39 @@ extension DataRequest {
         decoder.keyDecodingStrategy = .convertFromSnakeCase
         return cURLDescription(calling: { requestURL in
             #if DEBUG
-            SwiftyLogger.debug(requestURL)
+            if !requestURL.contains("api.splatnet3.com") {
+                SwiftyLogger.debug(requestURL)
+            }
             #endif
         })
         .validate({ request, response, data in
             DataRequest.ValidationResult(catching: {
+                /// 簡易的にアクセスしたURLの中身を返す
+                if let targetURL: URL = request?.url {
+                    SwiftyLogger.info("Request URL: \(targetURL)")
+                }
+                if let headers: HTTPHeaders = request?.headers {
+                    headers.forEach({ header in
+                        /// 認証に関するヘッダー以外を出力する
+                        if header.name != "Authorization" {
+                            SwiftyLogger.info("Request Headers: \(header)")
+                        } else {
+                            SwiftyLogger.info("Request Headers: \(header.name): Bearer \(Array(repeating: "*", count: max(0, header.value.count - 7)).joined())")
+                        }
+                    })
+                }
+                if let targetURL: URL = request?.url,
+                   let httpBody: Data = request?.httpBody,
+                   let body: [String: Any] = try? JSONSerialization.jsonObject(with: httpBody, options: [.json5Allowed]) as? [String: Any],
+                   targetURL.lastPathComponent == "graphql"
+                {
+                    body.forEach({ key, value in
+                        if key.contains("variables") {
+                            SwiftyLogger.info("Request Body: \(key): \(value)")
+                        }
+                    })
+                }
+
                 if let data = data {
                     if let failure = try? decoder.decode(Failure.NSO.self, from: data) {
                         throw failure
